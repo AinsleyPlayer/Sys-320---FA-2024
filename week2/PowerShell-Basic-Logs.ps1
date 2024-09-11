d21beb03ffe5c4e937d5412e7fac1dc4aecf56df
Ainsley <ainsley.smith@mymail.champlain.edu>
2024-09-11 19:06:30 -0400

    modified:   PowerShell-Basic-Logs.ps1 	new file:   Process-Management-1.ps1 	new file:   Process.csv 	new file:   Q4.ps1

--- week2/PowerShell-Basic-Logs.ps1
+++ week2/PowerShell-Basic-Logs.ps1
@@ -1,55 +1,55 @@
-﻿clear
+﻿<# clear
 # Q 1 
-# (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet0"}).IPAddress
+(Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet0"}).IPAddress
 
 # Q 2
 
-# (Get-NetIPAddress -AddressFamily  IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet0"}).PrefixLength
+(Get-NetIPAddress -AddressFamily  IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet0"}).PrefixLength
 
 # Q 3 & 4
 
-# Get-WmiObject -List | Where-Object { $_.Name -ilike "Win32_Net*" } | Sort-Object
+Get-WmiObject -List | Where-Object { $_.Name -ilike "Win32_Net*" } | Sort-Object
 
 # Q 5 & 6
 
-# Get-CimInstance Win32_NetworkAdapterConfiguration -Filter "DHCPEnabled=$true" | Select DHCPServer | Format-Table -HideTableHeaders
+Get-CimInstance Win32_NetworkAdapterConfiguration -Filter "DHCPEnabled=$true" | Select DHCPServer | Format-Table -HideTableHeaders
 
 # Q 7
 
-# (Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet0" }).ServerAddresses[0]
+(Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet0" }).ServerAddresses[0]
 
 # Q 8 
 
-#cd $PSScriptRoot
+cd $PSScriptRoot
 
-#$files=(Get-ChildItem)
-#$files
-#for ($j=0; $j -le $files.length; $j++){
-#   if ($files[$j].Name -ilike "*ps1"){
-#        Write-Host $files[$j].Name
-#   }
-#}
+$files=(Get-ChildItem)
+$files
+for ($j=0; $j -le $files.length; $j++){
+   if ($files[$j].Name -ilike "*ps1"){
+        Write-Host $files[$j].Name
+   }
+}
 
 # Q 9
 
-#$folderpath="PSScriptRoot\outfolder"
-#if (Test-Path $folderpath){
-#    Write-Host "Folder Already Exists"
-#}
-#else{
-#    New-Item -ItemType "directory" -Path $folderpath
-#}
+$folderpath="PSScriptRoot\outfolder"
+if (Test-Path $folderpath){
+    Write-Host "Folder Already Exists"
+}
+else{
+    New-Item -ItemType "directory" -Path $folderpath
+}
 
 # Q 10
 
 
-# cd $PSScriptRoot
-# $files=(Get-ChildItem)
+cd $PSScriptRoot
+$files=(Get-ChildItem)
 
-# $folderpath= "$PSScriptRoot/outfolder/"
-# $filePath = Join-Path $folderPath "out.csv"
+$folderpath= "$PSScriptRoot/outfolder/"
+$filePath = Join-Path $folderPath "out.csv"
 
-# $files | Where-Object {$files[$j].Name -ilike "*.ps1" } | Export-Csv -Path $filePath
+$files | Where-Object {$files[$j].Name -ilike "*.ps1" } | Export-Csv -Path $filePath
 
 # Q 11
 
@@ -57,3 +57,4 @@ cd $PSScriptRoot
 $files= Get-ChildItem -Recurse -File
 $files | Rename-Item -NewName { $_.name -Replace '.csv', '.log' }
 Get-ChildItem -Recurse -File 
+#>
---
+++ week2/Q4.ps1
@@ -0,0 +1,10 @@
+﻿# Q 4
+
+$chrome = Get-Process | Where-Object {$_.name -ilike "Chrome"}
+if($chrome -eq $null) {
+ Start-Process -FilePath chrome.exe 'https://champlain.edu'
+ }
+else {
+Stop-Process -name chrome
+
+}
