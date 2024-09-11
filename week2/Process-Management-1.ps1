714b41fd2df52302c3e3185f48fff2ab0fb40e63
AinsleyPlayer <90421152+AinsleyPlayer@users.noreply.github.com>
2024-09-11 19:11:24 -0400

    Update Process-Management-1.ps1

--- week2/Process-Management-1.ps1
+++ week2/Process-Management-1.ps1
@@ -1,18 +1,18 @@
 ﻿# Q 1
-# Get-Process -ProcessName C*| Where-Object Name -ilike 'C*'
+Get-Process -ProcessName C*| Where-Object Name -ilike 'C*'
 
 # Q 2
 
-# Get-Process | Where-Object {$_.Path -notlike 'system32*'}
+Get-Process | Where-Object {$_.Path -notlike 'system32*'}
 
 # Q 3
 
-<#
+
 $stopped = Get-Service | Where-Object {$_.Status -eq 'Stopped'} 
 $sort = $stopped | Sort-Object -Property Name
 $sort | Select-Object Status, Name, DisplayName | Export-Csv -Path "C:\Users\champuser\Sys-320---FA-2024\week2\Process.csv" -NoTypeInformation
-#>
 
+# Q 4
 
 $chrome = Get-Process | Where-Object {$_.name -ilike "Chrome"}
 if($chrome -eq $null) {
