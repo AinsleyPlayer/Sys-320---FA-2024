69c1ae12445b842acd90cd8ec4182a593080e931
Ainsley <ainsley.smith@mymail.champlain.edu>
2024-12-09 01:35:26 -0500

    commit

---
+++ week6/String-Helper.ps1
@@ -0,0 +1,47 @@
+﻿<# String-Helper
+*************************************************************
+   This script contains functions that help with String/Match/Search
+   operations. 
+************************************************************* 
+#>
+
+
+<# ******************************************************
+   Functions: Get Matching Lines
+   Input:   1) Text with multiple lines  
+            2) Keyword
+   Output:  1) Array of lines that contain the keyword
+********************************************************* #>
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+
+function checkPassword($password){
+    if($password.length -lt 6) {
+        return $false
+    }
+    elseif ($password -notmatch '.+[0-9].+'){
+        return $false
+      }
+    elseif ($password -notmatch '.+([a-z]|[A-Z]).+'){
+        return $false
+        }
+    elseif ($password -notmatch '.+[^A-Za-z0-9].+'){
+        return $false
+        }
+    else {
+        return $false
+    }
+    }
